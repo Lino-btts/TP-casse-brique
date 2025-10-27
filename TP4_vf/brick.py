d8e2685ba5f075be8e7ef75564507a0bd293a048
Lino-btts <lino.battesti@cpe.fr>
2025-10-27 19:41:22 +0100

    respect bonnes pratiques
    Added metadata and updated class initialization.

--- TP4_vf/brick.py
+++ TP4_vf/brick.py
@@ -1,12 +1,18 @@
-import tkinter as tk
+# codé en utf 8
+# Date : du 06/10/2025 au 27/10/2025 
+# Auteurs : Rayane Chekatt / Lino Battesti
+# Le programme permet de créer les briques normales et celles qui se cassent en deux collisions 
+# To do list : None
+
+from tkinter import Canvas
 
 class Brick:
     """Classe représentant une brique à casser."""
 
-    def __init__(self, canvas, x1, y1, x2, y2, color="orange", hits=None):
+    def __init__(self, canvas: Canvas, x1, y1, x2, y2, color="orange", hits=None):
         """
         Entrée :
-            - canvas (tk.Canvas) : zone de dessin
+            - canvas (Canvas) : zone de dessin
             - (x1, y1, x2, y2) (float) : coordonnées du rectangle
             - color (str) : couleur de la brique
             - hits (int) : nombre de coups nécessaires pour la casser
@@ -15,9 +21,12 @@ class Brick:
         """
         self.__canvas = canvas
         self.hits = hits
+
         # Création du rectangle sur le canvas
-        self.id = canvas.create_rectangle(x1, y1, x2, y2, fill=color,
-                                          width=1, outline="black")
+        self.id = canvas.create_rectangle(
+            x1, y1, x2, y2,
+            fill=color, width=1, outline="black"
+        )
 
     def coords(self):
         """Entrée : None | Sortie : liste [x1, y1, x2, y2] | But : retourner les coordonnées de la brique"""
